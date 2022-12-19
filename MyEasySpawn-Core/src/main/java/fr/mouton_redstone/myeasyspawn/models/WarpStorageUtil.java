76630d77d889fbef1980a2de6bde3350a18cff17
Le Mouton de Redstone <mouton.redstone@gmail.com>
2022-12-19 13:05:31 +0100

    Update WarpStorageUtil.java
    i'm dumb, so i deleted my stupid own class that was already exisitng in the bukkit library

--- MyEasySpawn-Core/src/main/java/fr/mouton_redstone/myeasyspawn/models/WarpStorageUtil.java
+++ MyEasySpawn-Core/src/main/java/fr/mouton_redstone/myeasyspawn/models/WarpStorageUtil.java
@@ -2,6 +2,8 @@ package fr.mouton_redstone.myeasyspawn.models;
 
 import com.google.gson.Gson;
 import fr.mouton_redstone.myeasyspawn.MyEasySpawn;
+import org.bukkit.Location;
+import org.bukkit.World;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,15 +13,20 @@ import java.util.*;
 
 public class WarpStorageUtil {
 
-    public static Map<String, CoordinatesSet> warps = new HashMap<>();
+    public static Map<String, Location> warps = new HashMap<>();
 
-    public static CoordinatesSet createWarp(String name, String world, float x, float y, float z) {
-        CoordinatesSet coords = new CoordinatesSet(world, x, y, z);
+    public static Location createWarp(String name, World world, float x, float y, float z) {
+        Location coords = new Location(world, x, y, z);
         warps.put(name, coords);
+        try {
+            saveWarps();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return coords;
     }
 
-    public static CoordinatesSet getWarp(String name){
+    public static Location getWarp(String name){
         if (warps.containsKey(name)) {
             return warps.get(name);
         }else{
@@ -29,22 +36,30 @@ public class WarpStorageUtil {
 
     public static void deleteWarp(String name){
         warps.remove(name);
+        try {
+            saveWarps();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static CoordinatesSet updateWarp(String name, CoordinatesSet newCoords){
+    public static Location updateWarp(String name, Location newCoords){
         if (warps.containsKey(name)){
-            CoordinatesSet coords = warps.get(name);
-            coords.setWorld(newCoords.getWorld());
-            coords.setX(newCoords.getX());
-            coords.setY(newCoords.getY());
-            coords.setZ(newCoords.getZ());
+            warps.replace(name, newCoords);
+            Location coords = warps.get(name);
+            try {
+                saveWarps();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return coords;
         }else{
             return null;
         }
+
     }
 
-    public static void saveNotes() throws IOException {
+    public static void saveWarps() throws IOException {
         Gson gson = new Gson();
         File file = new File(MyEasySpawn.getPlugin().getDataFolder().getAbsolutePath() + "/warps.json");
         file.getParentFile().mkdir();
